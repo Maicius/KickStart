35df143261eea6c5dbe1bcf9da27864f9fb70cdd
Maicius <maicius@outlook.com>
2019-11-12 21:25:31 +0800

    mancher算法

--- longestPalindrome.cpp
+++ longestPalindrome.cpp
@@ -3,11 +3,13 @@ using namespace std;
 
 class Solution {
 public:
+
+    // 中心拓展法
     string longestPalindrome(string s) {
         if (s.size() <= 1) {
             return s;
         }
-        string res = s.at[0];
+        string res = s.substr(0, 1);
         int max_len = 1;
         for (int i = 0; i < s.size() - 1; i++) {
             string odd = centerSpread(s, i, i);
@@ -32,4 +34,38 @@ public:
         }
         return s.substr(left + 1, right - left - 1);
     }
+
+    // 马拉车算法
+    int centerSpread(string str, int center) {
+        int left = center - 1;
+        int right = center + 1;
+        int step = 0;
+        while (left >= 0 && right <= str.size() && str[left] == str[right]){
+            left--;
+            right++;
+            step++;
+        }
+        return step;
+    }
+    string longestPalindrome2(string s) {
+        int size = s.size();
+        if (size < 2) return s;
+        string str = "#";
+        for (int i = 0; i < size; i++) {
+            str += s[i];
+            str += "#";
+        } 
+        int sSize = str.size();
+        int maxLen = 1;
+        int start = 0;
+        for (int i = 0; i < sSize; i++) {
+            int curLen = centerSpread(str, i);
+            if (curLen > maxLen) {
+                maxLen = curLen;
+                start = (i - maxLen) / 2;
+            }
+        }
+        return s.substr(start, maxLen);
+    }
+    
 };
