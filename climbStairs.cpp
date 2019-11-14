81e4aa8d12e9f18bd9b7a71eca485c5f5ed73450
Maicius <maicius@outlook.com>
2019-11-14 18:53:04 +0800

    爬楼梯

---
+++ climbStairs.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+/**
+ * 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+
+每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+
+注意：给定 n 是一个正整数。
+ */ 
+typedef long long ll;
+using namespace std;
+class Solution {
+public:
+    int climbStairs(int n) {
+        ll res = 0;
+        if (n <= 2) return n;
+        ll temp1 = 1, temp2 = 2;
+        for (int i = 3; i <= n; i++) {
+            res = temp1 + temp2;
+            temp1 = temp2;
+            temp2 = res;
+        }
+        return res;
+    }
+};
+
+int main() {
+    Solution *so = new Solution();
+    cout << so->climbStairs(100000) << endl; 
+}
