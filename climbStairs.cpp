bf9cc42e580b6b9296c050e1c48789f38aa81e8a
Maicius <maicius@outlook.com>
2019-11-14 19:21:13 +0800

    常规解法

--- climbStairs.cpp
+++ climbStairs.cpp
@@ -21,6 +21,18 @@ public:
         }
         return res;
     }
+
+    int climbStairs2(int n) {
+        ll res = 0;
+        if (n <= 2) return n;
+        int dp[n];
+        dp[0] = 1;
+        dp[1] = 2;
+        for (int i = 2; i < n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n - 1];
+    }
 };
 
 int main() {
